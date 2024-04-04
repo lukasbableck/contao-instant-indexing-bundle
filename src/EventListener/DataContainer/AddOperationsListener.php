86712a4f85bde84236aa22093c1eabf49cb21625
Lukas Bableck <lukas@bableck.dev>
2024-04-04 09:59:51 +0200

    Implement operations button in tl_page, tl_news, tl_calendar_events and tl_faq

--- src/EventListener/DataContainer/AddOperationsListener.php
+++ src/EventListener/DataContainer/AddOperationsListener.php
@@ -1,11 +1,21 @@
 <?php
 namespace LukasBableck\ContaoInstantIndexingBundle\EventListener\DataContainer;
 
+use Contao\Backend;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
+use Contao\Input;
+use Contao\NewsModel;
+use Contao\PageModel;
+use Contao\System;
+use LukasBableck\ContaoInstantIndexingBundle\Client\Google;
 
 #[AsHook('loadDataContainer')]
-class AddOperationsListener {
+class AddOperationsListener extends Backend {
+	public function __construct(private Google $googleClient) {
+	}
+
 	public function __invoke(string $table): void {
+		$request = System::getContainer()->get('request_stack')->getCurrentRequest();
 		if ('tl_page' !== $table && 'tl_news' !== $table && 'tl_calendar_events' !== $table && 'tl_faq' !== $table) {
 			return;
 		}
@@ -15,5 +25,48 @@ class AddOperationsListener {
 			'href' => 'key=indexGoogle',
 			'icon' => 'bundles/contaoinstantindexing/icons/google.svg',
 		];
+
+		if ('indexGoogle' === Input::get('key') && Input::get('id')) {
+			$page = null;
+			$rootPage = null;
+			$model = null;
+			switch ($table) {
+				case 'tl_page':
+					$page = PageModel::findWithDetails(Input::get('id'));
+					break;
+				case 'tl_news':
+					$model = NewsModel::findByPk(Input::get('id'));
+					break;
+				case 'tl_calendar_events':
+					$model = CalendarEventsModel::findByPk(Input::get('id'));
+					break;
+				case 'tl_faq':
+					$model = FaqModel::findByPk(Input::get('id'));
+			}
+
+			if (null !== $model && null === $page) {
+				$archive = $model->getRelated('pid');
+				if (null !== $archive) {
+					$page = $archive->getRelated('jumpTo')->loadDetails();
+					$rootPage = PageModel::findByPk($page->rootId);
+					if (null !== $page) {
+						$url = $page->getAbsoluteUrl('/'.$model->alias);
+					}
+				}
+			} elseif (null === $model && null !== $page) {
+				$rootPage = PageModel::findByPk($page->rootId);
+				$url = $page->getAbsoluteUrl();
+			} else {
+				return;
+			}
+
+			if (null === $rootPage || !$rootPage->googleServiceAccountJSON) {
+				return;
+			}
+
+			$this->googleClient->publish($url, html_entity_decode($rootPage->googleServiceAccountJSON));
+
+			$this->redirect($this->getReferer());
+		}
 	}
 }
--- src/EventListener/DataContainer/PageListener.php
+++ src/EventListener/DataContainer/PageListener.php
@@ -1,6 +1,7 @@
 <?php
 namespace LukasBableck\ContaoInstantIndexingBundle\EventListener\DataContainer;
 
+use Contao\Backend;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
 use Contao\DataContainer;
 use Contao\Input;
@@ -8,14 +9,13 @@ use Contao\PageModel;
 use Contao\System;
 use LukasBableck\ContaoInstantIndexingBundle\Client\Google;
 
-class PageListener {
+class PageListener extends Backend{
 	public function __construct(private Google $googleClient) {
 	}
 
 	#[AsCallback(table: 'tl_page', target: 'select.buttons')]
 	public function addIndexButton(array $arrButtons, DataContainer $dc): array {
 		if (null !== Input::post('index_google') && 'tl_select' == Input::post('FORM_SUBMIT')) {
-			$router = System::getContainer()->get('router');
 			$objSession = System::getContainer()->get('request_stack')->getSession();
 			$session = $objSession->all();
 			$ids = $session['CURRENT']['IDS'] ?? [];
@@ -30,12 +30,10 @@ class PageListener {
 					continue;
 				}
 
-				$dc->id = $id;
-				$dc->activeRecord = $objPage;
-
 				$pageUrl = $objPage->getAbsoluteUrl();
-				$this->googleClient->index($pageUrl, html_entity_decode($rootPage->googleServiceAccountJSON));
+				$this->googleClient->publish($pageUrl, html_entity_decode($rootPage->googleServiceAccountJSON));
 			}
+			$this->redirect($this->getReferer());
 		}
 
 		$arrButtons['indexGoogle'] = '<button type="submit" name="index_google" id="index_google" class="tl_submit" accesskey="i">'.$GLOBALS['TL_LANG']['MSC']['indexGoogleSelected'].'</button> ';
